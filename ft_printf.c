4190a56ea1ad645e9b742d95e859eaf51870d6d3
Taha Chtaibi <tchtaibi@e2r2p1.1337.ma>
2021-11-22 15:41:58 +0100

    Mon 3:41Pm 22 Nov

--- ft_printf.c
+++ ft_printf.c
@@ -6,52 +6,54 @@
 /*   By: tchtaibi <tchtaibi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 04:19:11 by tchtaibi          #+#    #+#             */
-/*   Updated: 2021/11/22 15:31:08 by tchtaibi         ###   ########.fr       */
+/*   Updated: 2021/11/22 15:41:37 by tchtaibi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-void ft_print
+
+void ft_printcent(char c, va_list arguments)
+{
+    if(c == 'c')
+        ft_putchar(va_arg(arguments, int));
+    if(c == 's')
+        ft_putstr(va_arg(arguments, char*));
+    if(c == 'p')
+    {
+        t_putstr("0x");
+        ft_printhexlow(va_arg(arguments,int));
+    }
+    if(c == 'd' || c == 'i')
+        ft_putnbr(va_arg(arguments, int));
+    if(c == 'u')
+    ft_putnbr((unsigned int) va_arg(arguments,unsigned int));
+    if(c == 'x')
+        ft_printhexlow(va_arg(arguments,int));
+    if(c == 'X')
+        ft_printhexup(va_arg(arguments,int));
+    if(c == '%')
+        ft_putchar('%');
+}
 int ft_printf(const char *format, ...)
 {
     char *str;
-    int nbr;
     va_list arguments;
-    int lenght;
 
     lenght = 0;
 	
-    va_start ( arguments, format );
+    va_start ( arguments, format);
     str = (char *)format;
     while(*str)
     {
         if(*str == '%')
-            {
-                str++;
-                if(*str == 'c')
-                    ft_putchar(va_arg(arguments, int));
-                if(*str == 's')
-                    ft_putstr(va_arg(arguments, char*));
-                if(*str == 'p')
-                {
-                    ft_putstr("0x");
-                    ft_printhexlow(va_arg(arguments,int));
-                }
-                if(*str == 'd' || *str == 'i')
-                    ft_putnbr(va_arg(arguments, int));
-                if(*str == 'u')
-                    ft_putnbr((unsigned int) va_arg(arguments,unsigned int));
-                if(*str == 'x')
-                    ft_printhexlow(va_arg(arguments,int));
-                if(*str == 'X')
-                    ft_printhexup(va_arg(arguments,int));
-                if(*str == '%')
-                    ft_putchar('%');
-            }
+        {
+            str++;
+            ft_printcent(*str, arguments);
+        }
         else
             ft_putchar(*str);
         str++;
     }
     va_end (arguments);
-    return lenght;
+    return 0;
 }
